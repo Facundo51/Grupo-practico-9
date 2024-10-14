47da37f0b2e740308a117f0498c8bb91697b291d
wachidota <JO341831@fi365.ort.edu.uy>
2024-10-14 09:44:09 -0300

    asdasd

--- Classes/Sistema.cs
+++ Classes/Sistema.cs
@@ -16,14 +16,13 @@ namespace Classes
 
         public void agregarCliente(int ci, string nombre, string telefono)
         {
-            try { Cliente nuevo = new Cliente(ci, nombre, telefono); 
+             Cliente nuevo = new Cliente(ci, nombre, telefono); 
                 if(!(_clientes.Contains(nuevo)))
                 {
                     _clientes.Add(nuevo);
                 }
             }
-            catch (Exception ex) { Console.WriteLine(ex.Message); }
+           
 
         }
     }
-}
