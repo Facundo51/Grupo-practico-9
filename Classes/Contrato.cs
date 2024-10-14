7f155916d1045df864b054f7481b85ec807452e4
Facundo <facufernandez974@example.com>
2024-10-14 09:57:59 -0300

    En progreso

--- Classes/Contrato.cs
+++ Classes/Contrato.cs
@@ -12,14 +12,22 @@ namespace Classes
         private Cliente _cliente;
         private Excursion _excursion;
         private int _cantidadPasajeros;
-        private int _menores;
+        private int _cantMenores;
         private decimal _precio;
 
         public Contrato(
-            
+            Cliente cliente,
+            Excursion excursion,
+            int cantidadPasajeros,
+            int cantMenores,
+            decimal precio
             )
         {
-
+            _cliente = cliente;
+            _excursion = excursion;
+            _cantMenores = cantidadPasajeros;
+            _cantidadPasajeros = cantidadPasajeros;
+            _precio = precio;
         }
     }
 }
---
+++ Classes/Extranjero.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Extranjero : Excursion
+    {
+        private bool _pasaje;
+        private decimal s_costoSecuro;
+    }
+}
