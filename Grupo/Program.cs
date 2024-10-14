060a0eee4c4b2b0a9d400a47b5db4c906bebf072
Facundo <facufernandez974@example.com>
2024-10-14 09:47:36 -0300

    iteracion 4

--- Grupo/Program.cs
+++ Grupo/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Grupo
+﻿using Classes;
+
+namespace Grupo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Sistema sistema = new Sistema();
 
+            Console.WriteLine("Ingresa la cedula");
+            int.TryParse(Console.ReadLine(), out int cedula);
+
+            Console.WriteLine("Ingresa el nombre");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingresa el telefono");
+            string telefono = Console.ReadLine();
+
+            try
+            {
+                sistema.agregarCliente(cedula, nombre, telefono);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
